67832fad8d6e4396309c7aafc85dba5a95dcd4db
MatthewTurney <matthew.w.turney@gmail.com>
2016-05-14 12:03:15 -0700

    ;)

---
+++ Roll-a-ball - Copy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public bool grounded = true;
+    public float jumpPower = 100;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    void onCollissionEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+        if (Input.GetKeyDown("space") && grounded == true)
+        {
+            Vector3 jumpVector = new Vector3(0.0f, jumpPower, 0.0f);
+            rb.AddForce(jumpVector);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >=12)
+        {
+            winText.text = "You Win!!";
+        }
+    }
+}
