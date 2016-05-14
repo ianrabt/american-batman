d0cb2a2654208fec0778e7d211c722bb5a40143e
MatthewTurney <matthew.w.turney@gmail.com>
2016-05-14 13:03:42 -0700

    Version Control on Unity

--- Roll-a-ball - Copy/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball - Copy/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,23 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
-    private Rigidbody rb;
+    private CharacterController rb;
     private int count;
     public Text countText;
     public Text winText;
     public bool grounded = true;
     public float jumpPower = 100;
 
+    public float rotationSpeed = 5;
+    public float direction = 0;
+
+    private CharacterController controller;
+    private float speed = 6;
+    private float turnSpeed = 90;
+
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<CharacterController>();
         count = 0;
         setCountText();
         winText.text = "";
@@ -31,10 +37,17 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
+        // float moveHorizontal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
+        // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //rb.AddForce(movement * speed);
+
+        Vector3 moveDir = new Vector3(0.0f, 0.0f, 0.0f);
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+        movDir = transform.forward * Input.GetAxis("Vertical") * speed;
+        controller.Move(movDir * Time.deltaTime - Vector3.up * 0.1);
+
         if (Input.GetKeyDown("space") && grounded == true)
         {
             Vector3 jumpVector = new Vector3(0.0f, jumpPower, 0.0f);
